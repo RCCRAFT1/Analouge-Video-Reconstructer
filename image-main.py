82da087cded4a41afeb116733d1a581333e7fff4
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2020-04-10 21:57:58 -0700

    Added the nural net!

---
+++ image-main.py
@@ -0,0 +1,10 @@
+can, IT
+for it, run test
+    {com2} readline0-15 then 16-220
+    {
+        int interchange; load for analouge 330-400 read; in bash for 0
+        load image0 then (random) frames, count frame 0; int3
+        int
+    }
+stitch image0 then frame0; from int-in
+stitch, display
